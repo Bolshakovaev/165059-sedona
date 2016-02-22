e3174ceb54343dd02ea81fbfac9c91218d01665d
Bolshakovaev <bolshakova_ev@mail.ru>
2016-02-22 23:25:59 +0300

    Всё исправила

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-<script>
 var link = document.querySelector(".booking h3");
-var popup = document.querySelector(".booking");
- var form = popup.querySelector("form");
+var popupform = document.querySelector(".booking-form");
 
-var checkin = popup.querySelector("[name=check-in]");
+var checkin = popupform.querySelector("[name=check-in]");
 
-var checkout = popup.querySelector("[name=check-out]");
+var checkout = popupform.querySelector("[name=check-out]");
 
 
-link.addEventListener("click", function(event) {
-event.preventDefault();
-    
-popup.classList.add("booking-form-show");  
- checkin.focus();   
+link.addEventListener("click", function() {
+    if (popupform.classList.contains('booking-form-show')) {
+		popupform.classList.remove("booking-form-show");  
+	} else {
+		popupform.classList.add("booking-form-show");  
+		checkin.focus();   
+	}
 });
-  form.addEventListener("submit", function(event) {
-       
-      if (!checkin.value || !checkout.value) {
-          event.preventDefault();
-          console.log("Нужно ввести даты заезда и даты выезда"); 
-        }
-      
-      });
-</script>
+
+popupform.addEventListener("submit", function(event) {
+    if (!checkin.value || !checkout.value) {
+        event.preventDefault();
+        alert("Нужно ввести даты заезда и даты выезда"); 
+    }
+});
