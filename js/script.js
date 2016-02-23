76f489fc2013bcc8e8965b73491e1c600fd61cba
Bolshakovaev <bolshakova_ev@mail.ru>
2016-02-24 02:29:05 +0300

    Внесла исправления по результатам финальной проверки

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 var link = document.querySelector(".booking h3");
 var popupform = document.querySelector(".booking-form");
-
 var checkin = popupform.querySelector("[name=check-in]");
-
 var checkout = popupform.querySelector("[name=check-out]");
 
-
 link.addEventListener("click", function() {
-    if (popupform.classList.contains('booking-form-show')) {
-		popupform.classList.remove("booking-form-show");  
-	} else {
-		popupform.classList.add("booking-form-show");  
-		checkin.focus();   
+  if (popupform.classList.contains('booking-form-show')) {
+  popupform.classList.remove("booking-form-show");  
+  } else {
+	popupform.classList.add("booking-form-show");  
+	checkin.focus();   
 	}
 });
-
 popupform.addEventListener("submit", function(event) {
-    if (!checkin.value || !checkout.value) {
-        event.preventDefault();
-        alert("Нужно ввести даты заезда и даты выезда"); 
+  if (!checkin.value || !checkout.value) {
+  event.preventDefault();
+  alert("Нужно ввести даты заезда и даты выезда"); 
     }
 });
