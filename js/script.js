f318de874bb383a04e4cee15c8743dd91e1dd329
Bolshakovaev <bolshakova_ev@mail.ru>
2016-02-24 08:00:43 +0300

    Внесла изменения по результатам проверки

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-var link = document.querySelector(".booking h3");
-var popupform = document.querySelector(".booking-form");
-var checkin = popupform.querySelector("[name=check-in]");
-var checkout = popupform.querySelector("[name=check-out]");
+if (document.querySelector('.homepage')) {
+	var link = document.querySelector(".booking h3");
+	var popupform = document.querySelector(".booking-form");
+	var checkin = popupform.querySelector("[name=check-in]");
+	var checkout = popupform.querySelector("[name=check-out]");
 
-link.addEventListener("click", function() {
-  if (popupform.classList.contains('booking-form-show')) {
-  popupform.classList.remove("booking-form-show");  
-  } else {
-	popupform.classList.add("booking-form-show");  
-	checkin.focus();   
-	}
-});
-popupform.addEventListener("submit", function(event) {
-  if (!checkin.value || !checkout.value) {
-  event.preventDefault();
-  alert("Нужно ввести даты заезда и даты выезда"); 
-    }
-});
+	link.addEventListener("click", function() {
+		if (popupform.classList.contains('booking-form-show')) {
+		popupform.classList.remove("booking-form-show");  
+		} else {
+		popupform.classList.add("booking-form-show");  
+		checkin.focus();   
+		}
+	});
+	popupform.addEventListener("submit", function(event) {
+		if (!checkin.value || !checkout.value) {
+		event.preventDefault();
+		alert("Нужно ввести даты заезда и даты выезда"); 
+			}
+	});
+	
+}
